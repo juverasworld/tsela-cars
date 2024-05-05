63dce7c6c3f0ea380623e1a75a5fdcf2246c1070
Vee <ifebuchejulia@gmail.com>
2024-05-05 23:37:56 +0100

    CONTENT UPDATE

--- js.js
+++ js.js
@@ -508,3 +508,31 @@ TweenMax.from('.product-desc button', 3, {
     y: -50,
     ease: Expo.easeInOut
 })
+
+
+gsap.fromTo(
+    ".loading-page",
+    { opacity: 1 },
+    {
+      opacity: 0,
+      display: "none",
+      duration: 1.5,
+      delay: 3.5,
+    }
+  );
+  
+  gsap.fromTo(
+    ".logo-name",
+    {
+      y: 50,
+      opacity: 0,
+    },
+    {
+      y: 0,
+      opacity: 1,
+      duration: 2,
+      delay: 0.5,
+    }
+  );
+  
+  
