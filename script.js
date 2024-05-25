4c02beda063ea0c1f062624c890b54ec1874593e
Vee <ifebuchejulia@gmail.com>
2024-05-25 11:39:41 +0100

    content update

---
+++ script.js
@@ -0,0 +1,72 @@
+var menuToggle = document.getElementById("menuToggle");
+
+var menuBar = gsap.timeline();
+
+menuBar.to('.bar-1', 0.5,{
+	attr:{d: "M8,2 L2,8"},
+	x:1,
+	ease: Power2.easeInOut
+}, 'start')
+
+menuBar.to('.bar-2', 0.5,{
+	autoAlpha: 0
+}, 'start')
+
+menuBar.to('.bar-3', 0.5,{
+	attr:{d: "M8,8 L2,2"},
+	x:1,
+	ease: Power2.easeInOut
+}, 'start')
+
+menuBar.reverse();
+
+
+var tl = gsap.timeline({ paused: true});
+
+tl.to('.fullpage-menu', {
+	duration:0,
+	display: "block",
+	ease: 'Expo.easeInOut',
+});
+
+tl.from('.menu-bg span', {
+	duration:1,
+	x:"100%",
+	stagger: 0.1,
+	ease: 'Expo.easeInOut'
+});
+
+tl.from('.main-menu li a', {
+	duration:1.5,
+	y:"100%",
+	stagger: 0.2,
+	ease: 'Expo.easeInOut'
+} , "-=0.5");
+
+tl.from('.social-links li', {
+	duration:1,
+	y:"-100%",
+	opacity:0,
+	stagger: 0.1,
+	ease: 'Expo.easeInOut'
+} , "-=0.5");
+
+tl.reverse();
+
+menuToggle.addEventListener('click', function(){
+	menuBar.reversed(!menuBar.reversed());
+	tl.reversed(!tl.reversed());
+});
+
+
+
+// Reversing the marquee animation on scroll
+window.addEventListener("scroll", function() {
+    let content = document.querySelector(".content");
+    let scrollDirection = window.scrollY > this.oldScroll ? "down" : "up";
+    gsap.to(content, {
+        x: scrollDirection === "down" ? -100 : 0,
+        duration: 1
+    });
+    this.oldScroll = window.scrollY;
+}, { passive: true });
